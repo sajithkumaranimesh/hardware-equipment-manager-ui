28e8be15ea63ab3142d1d8d7f89ede77a1923aab
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-09-25 19:13:33 +0530

    create the sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import SideBar from "./components/common/SideBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 export default function App() {
-  return true;
+  return (
+    <SideBar/>
+  );
 }
 
 
